e8774b0b65873d14c4804603ec4e115ed0344448
karan singh <luckykaran95@gmail.com>
2019-01-29 19:39:45 +0530

    first commit

---
+++ DesignPatterns.cs
@@ -0,0 +1,43 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DesignPatterns.cs" company="CompanyName">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace DesignPatterns
+{
+    using System;
+
+    /// <summary>
+    /// Design pattern class is calling all the 
+    /// class to execute their methods separately
+    /// </summary>
+    public class DesignPatterns
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+            char ch;
+            ////starting do while iteration
+            do
+            {
+                Console.WriteLine("Enter 1 to execute First singleton program");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                ////switch case to take to your desired class
+                switch (choice)
+                {
+                    case 1:
+                        SingletonEx fromStudent = SingletonEx.GetInstance;
+                        fromStudent.Execution();
+                        break;
+                }
+
+                Console.WriteLine("Do you want to execute more programs(y/n)");
+                ch = Convert.ToChar(Console.ReadLine());
+            }
+            while (ch != 'n');   
+        }
+    }
+}
---
+++ SingletonEx.cs
@@ -0,0 +1,71 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="SingletonEx.cs" company="CompanyName">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace DesignPatterns
+{
+    using System;
+
+    /// <summary>
+    /// Singleton class is just demo of not creating more than one instance 
+    /// sealed is a keyword use to restrict the inheritance inside nested class or outside the class
+    /// </summary>
+    public sealed class SingletonEx
+    {
+        /// <summary>
+        /// The counter
+        /// </summary>
+        private static int counter = 0;
+
+        /// <summary>
+        /// The instance of singleton is set null 
+        /// so that we can avoid creation of more tha one instance
+        /// </summary>
+        private static SingletonEx instance = null;
+
+        /// <summary>
+        /// Gets the get instance.
+        /// </summary>
+        /// <value>
+        /// The get instance will return the instance 
+        /// only if the value of instance is set to null
+        /// </value>
+        public static SingletonEx GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    ////creating instance of the class
+                    instance = new SingletonEx();
+                }
+
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// Messages method will be printed as many
+        /// times we calling with different instance of the class
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public void Message(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        /// <summary>
+        /// Executions method is declared to call the methods by reference of the class.
+        /// </summary>
+        public void Execution()
+        {
+            ////creating first reference
+            SingletonEx fromStudent = SingletonEx.GetInstance;
+            fromStudent.Message("From student");
+            ////creating second reference
+            SingletonEx fromEmpoyee = SingletonEx.GetInstance;
+            fromStudent.Message("From employee");          
+        }
+    }
+}
